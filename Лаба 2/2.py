f25be1783dfc2efdd898cde3e4689f814b05b6e3
M1per <Dubrovskikhars@gmail.com>
2025-02-18 10:17:18 +0500

    Update 2.py

--- Лаба 2/2.py
+++ Лаба 2/2.py
@@ -1,7 +1,10 @@
-N = 5**36 + 5**24 - 25
-c = 0
-while N > 0:
-    if N % 5 == 4:
-        c += 1
-    N //= 5
-print(c)
+def count_four():
+    N = 5**36 + 5**24 - 25
+    c = 0
+    while N > 0:
+        if N % 5 == 4:
+            c += 1
+        N //= 5
+    return(c)
+redult = count_four()
+print(result)

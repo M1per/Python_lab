1dcaeca1333a51365f730cbf681c7949c2f2e7ef
M1per <Dubrovskikhars@gmail.com>
2025-03-03 16:24:04 +0500

    Add files via upload

---
+++ Лаба 3/1.py
@@ -0,0 +1,25 @@
+# Функция без рекурсии
+def intersect(list1, list2):
+    return [item for item in list1 if item in list2]
+
+# Функция с рекурсией
+def intersect_recursive(list1, list2):
+    if not list1:
+        return []
+    if list1[0] in list2:
+        return [list1[0]] + intersect_recursive(list1[1:], list2)
+    else:
+        return intersect_recursive(list1[1:], list2)
+
+# Примеры использования
+print(intersect([1, 2, 3, 4], [2, 3, 4, 6, 8]))
+print(intersect([5, 8, 2], [2, 9, 1]))
+print(intersect([5, 8, 2], [7, 4]))
+
+print(intersect_recursive([1, 2, 3, 4], [2, 3, 4, 6, 8]))
+print(intersect_recursive([5, 8, 2], [2, 9, 1]))
+print(intersect_recursive([5, 8, 2], [7, 4]))
+
+
+
+
---
+++ Лаба 3/2.py
@@ -0,0 +1,19 @@
+import math
+
+def calculate_sqrt_no(n):
+    result = 0
+    for _ in range(n):
+        result = math.sqrt(3 + result)
+    return result
+
+
+
+def calculate_sqrt_yes(n):
+    if n == 0:
+        return 0
+    return math.sqrt(3 + calculate_sqrt_yes(n - 1))
+
+# Ввод количества корней от пользователя
+n = int(input("Введите количество корней: "))
+print(calculate_sqrt_yes(n))
+print( calculate_sqrt_no(n))

4b1814be1724a4e84b81ed4e7fdd4602ccd586fc
M1per <Dubrovskikhars@gmail.com>
2025-03-24 15:56:15 +0500

    Add files via upload

---
+++ Лаба 5/1.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def password_generator(length=8):
+    chars = random.choices(string.ascii_letters + string.digits, k=length)
+    password = ''.join(chars)
+    inverted_password = ''.join(map(lambda c: c.swapcase(), password))
+    return inverted_password
+
+# Пример использования (генерируем 5 паролей)
+for x in range(5):
+    print(password_generator())

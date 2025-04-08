c8625302e95030e6a931479349d27c62c10adcf4
M1per <Dubrovskikhars@gmail.com>
2025-04-08 10:06:17 +0500

    Update 1.py

--- Лаба 5/1.py
+++ Лаба 5/1.py
@@ -2,10 +2,12 @@ import random
 import string
 
 def password_generator(length=8):
-    chars = random.choices(string.ascii_letters + string.digits, k=length)
-    password = ''.join(chars)
-    inverted_password = ''.join(map(lambda c: c.swapcase(), password))
-    return inverted_password
+    for x in range(5):
+        chars = random.choices(string.ascii_letters + string.digits, k=length)
+        password = ''.join(chars)
+        yield password
 
-for x in range(5):
-    print(password_generator())
+passwords = map(lambda p: ''.join(c.swapcase() for c in p), password_generator())
+
+for password in passwords:
+    print(password)
